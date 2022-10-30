436ae9538636a45eb5c79e20092f237d9ed693af
MTalha <talhatarar19@gmail.com>
2022-10-30 22:54:04 +0500

    basics

---
+++ basic.py
@@ -0,0 +1,15 @@
+print("This is my first python program");
+
+##print("This is my second "program"")
+#when your printing value is also in double quotes you should follow this method
+
+print('This is my second "program"');
+
+#But if you have an both single and double qoutes we should follow this method
+
+print('That\'s my third "program"')
+
+#if we want to print name many time 
+
+print('TALHA'+'TALHA');
+print(10*'TALHA');
