c1123606da4046314a4ec86aa05a51354a3582b1
MTalha <talhatarar19@gmail.com>
2022-11-10 08:21:13 +0500

    data-types

---
+++ data-types.py
@@ -0,0 +1,13 @@
+# DATA TYPES
+# Text Type :str
+# Sets Type :Set,frozenset 
+# Numeric Type :int,float,comlex
+# boolean Type :bool
+# Mapping Type :dic
+# Sequence Type:list,tuple
+
+# Text Type 
+x="Talha";
+print(x);
+print(type(x)); #<class 'str'>
+#ALL types discuss seperately
